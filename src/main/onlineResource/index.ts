3a6a2ba1f5bda19a2ce69505e42a80cde9c651df
lyswhut <lyswhut@qq.com>
2026-05-17 10:05:24 +0800

    feat: enhance logging for music URL retrieval and proxy creation

--- src/main/onlineResource/index.ts
+++ src/main/onlineResource/index.ts
@@ -1,12 +1,14 @@
 import { getMusicUrl } from '../isolate'
-import { configuration, musicUtils, registerResourceAction } from '../shared/hostAPI'
+import { configuration, console, musicUtils, registerResourceAction } from '../shared/hostAPI'
 
 export const initOnlineResource = async () => {
   registerResourceAction({
     async musicUrl(params) {
       const quality = params.quality || '128k'
       let url = await getMusicUrl(params.musicInfo, quality)
+      console.log(`${params.musicInfo.name}, ${params.musicInfo.meta.source}, ${quality}: ${url}`)
       url = await musicUtils.createProxyUrl(url, {}, (await configuration.getConfigs<[boolean]>(['enabledCache']))[0])
+      console.log(`proxy: ${url}`)
       return { quality, url }
     },
   })
